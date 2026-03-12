c4c67d1fbce681047b0c27bdcbcd8e5104b365ab
parveenganugaparveen-sudo <parveenganugaparveen@gmail.com>
2026-03-12 12:20:39 +0530

    Add files via upload

---
+++ ifstatement.js
@@ -0,0 +1,22 @@
+//let age=19;
+//let d1=false
+//if (age>18||d1==true){
+    ///console.log("you can  eligible for vote");
+//}
+//else{
+    ///console.log("you can't eligible")
+//}
+let adhar=false;
+let pan=true;
+if(adhar){
+    console.log("you can create the account");
+}
+else if(pan){
+    console.log("you can create account");
+}
+else{
+console.log("you cannot create the account");
+}
+
+
+
