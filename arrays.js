e02363549f1a6ee8c5d6e0f5e2ff59120d1392db
parveenganugaparveen-sudo <parveenganugaparveen@gmail.com>
2026-03-13 11:53:53 +0530

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,17 @@
+// let fruits=['custord apple','banana','mango','dragon fruit' ];
+// console.log(fruits[3]);
+// console.log(fruits[2]);
+
+
+
+// let arr = [1,2,3,4,5,6,];
+// arr.pop(5);
+// console.log(arr);
+
+// let arr = [,2,3,4,5,6,7];
+// arr.shift();
+// console.log(arr);
+
+
+let arr = [1,2,3,4,5,6,7,8,9,76,0]
+console.log(arr.length);
