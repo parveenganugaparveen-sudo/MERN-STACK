81a74d6fd963bc6526e531898b92ea189e507a58
parveenganugaparveen-sudo <parveenganugaparveen@gmail.com>
2026-03-13 11:54:13 +0530

    Add files via upload

---
+++ object.js
@@ -0,0 +1,14 @@
+// const person={
+//     name:"Sweety",
+//     age:20,
+//     greet:function(){
+//         return "hello,my name is $ {this.name}";
+//         }    }
+//         console.log(person .name)
+//          console.log(person .age)
+
+const person={Name:"Sweety",age:"20",city:"indore"};
+const{Name,age,city}=person;
+console.log(Name);
+console.log(age);
+console.log(city);
